e5faa1cbe0162bfb6ddff93d9bc4ec03aef1cdca
Mohamad Masri <Mohamadmasri123>
2024-12-07 17:01:28 +0200

    edit 2

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,9 @@ const App = () => {
        <Navbar/>
       <BrowserRouter>
         <Routes>   
-        <Route path="/" element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
 
            <Route path="/" element={<Services/>}/>    
-                <Route path="/home" element={<Home/>}/>
 
            <Route path="/Contact" element={<Contact/>}/>
            <Route path="/About" element={<About/>}/>
